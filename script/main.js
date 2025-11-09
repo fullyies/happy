11c9d98c9c88f615aebbc14860f09278c5a609ed
fullyies <randigans2@gmail.com>
2025-11-09 17:27:23 +0700

    Update background music prompt and animation

--- script/main.js
+++ script/main.js
@@ -1,13 +1,13 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: 'nk nyetel lagu di background e dak? same bai sih',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
+        confirmButtonText: 'aok',
+        cancelButtonText: 'dak',
     }).then((result) => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
@@ -269,4 +269,5 @@ const animationTimeline = () => {
     replyBtn.addEventListener("click", () => {
         tl.restart();
     });
+
 }
